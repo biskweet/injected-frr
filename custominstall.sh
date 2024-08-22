20e4a63689d272da6d85c4505a859417a21ca79d
Biskweet <67809939+biskweet@users.noreply.github.com>
2024-08-22 14:40:07 +0900

    Create custominstall.sh

---
+++ custominstall.sh
@@ -0,0 +1,28 @@
+apt install libgrpc++-dev protobuf-compiler-grpc libsqlite3-dev libzmq5 libzmq3-dev
+groupadd -r -g 92 frr
+groupadd -r -g 85 frrvty
+adduser --system --ingroup frr --home /var/run/frr/ \
+   --gecos "FRR suite" --shell /sbin/nologin frr
+usermod -a -G frrvty frr
+./bootstrap.sh
+./configure \
+    --prefix=/usr \
+    --includedir=\${prefix}/include \
+    --bindir=\${prefix}/bin \
+    --sbindir=\${prefix}/lib/frr \
+    --libdir=\${prefix}/lib/frr \
+    --libexecdir=\${prefix}/lib/frr \
+    --sysconfdir=/etc \
+    --localstatedir=/var \
+    --with-moduledir=\${prefix}/lib/frr/modules \
+    --enable-configfile-mask=0640 \
+    --enable-logfile-mask=0640 \
+    --enable-snmp=agentx \
+    --enable-multipath=64 \
+    --enable-user=frr \
+    --enable-group=frr \
+    --enable-vty-group=frrvty \
+    --with-pkg-git-version \
+    --with-pkg-extra-version=-MyOwnFRRVersion
+make
+make install
